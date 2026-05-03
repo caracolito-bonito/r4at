a440e3044a4cc5d820c383c3bd10500135a978a1
Sergey Kupriyanov <xvoztuk@gmail.com>
2026-05-03 17:02:33 +0400

    use mpsc to handle client-server interaction, implemented client side

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::fmt::Display;
-use std::io::Write;
-use std::net::TcpListener;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::{Receiver, Sender, channel};
+use std::thread;
 
 type Result<T> = std::result::Result<T, ()>;
 
@@ -18,9 +20,15 @@ impl<T: Display> Display for Sensitive<T> {
         if SENSITIVE_MODE.load(Ordering::Relaxed) {
             writeln!(f, "[REDACTED]")
         } else {
-            writeln!(f, "{}", self.0)
+            self.0.fmt(f)
         }
     }
+}
+
+enum Message {
+    ClientConnected,
+    ClientDisconnected,
+    NewMessage(Vec<u8>),
 }
 
 fn main() -> Result<()> {
@@ -29,11 +37,18 @@ fn main() -> Result<()> {
     let listener = TcpListener::bind(addr)
         .map_err(|err| eprintln!("ERROR: cound not bind {addr}: {}", Sensitive(err)))?;
     set_sensitive_mode(false);
-    println!("Listening to {addr}");
+
+    println!("Listening to {}", Sensitive(addr));
+
+    let (message_sender, message_recevier): (Sender<Message>, Receiver<Message>) = channel();
+
+    thread::spawn(|| server(message_recevier));
+
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let _ = writeln!(stream, "Hola, cabroncito");
+            Ok(stream) => {
+                let message_sender = message_sender.clone();
+                thread::spawn(|| client(stream, message_sender));
             }
             Err(e) => {
                 eprintln!("ERROR: could not accept connection: {e}")
@@ -42,4 +57,27 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn server(_messages: Receiver<Message>) -> Result<()> {
+    todo!()
+}
+
+fn client(mut stream: TcpStream, messages: Sender<Message>) -> Result<()> {
+    messages
+        .send(Message::ClientConnected)
+        .map_err(|err| eprintln!("ERROR: Could not send message to the server thread: {err}"))?;
+
+    let mut buffer = Vec::with_capacity(64);
+    loop {
+        let bytes_read = stream.read(&mut buffer).map_err(|err| {
+            eprintln!("ERROR: Could not read message from client {err}");
+            let _ = messages.send(Message::ClientDisconnected);
+        })?;
+        messages
+            .send(Message::NewMessage(buffer[0..bytes_read].to_vec()))
+            .map_err(|err| {
+                eprintln!("ERROR: Failed to send a message to the server thread: {err}");
+            })?;
+    }
 }
