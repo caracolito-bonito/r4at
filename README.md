ea8e5a8750a7efe1d31538cd6aed9b2b0d39af79
Sergey Kupriyanov <xvoztuk@gmail.com>
2026-05-24 16:22:22 +0400

    now it can render user input and the ability to exit using control + c

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 1. Follow [tsoding tutorial](https://www.youtube.com/watch?v=BbIEuNscn_E) and implement simple multi-user chat using only standard library ⏳
   - server, client, auth, rate limiting - ✅
-  - tls and security - ⏳
-2. Rewrite it using some external crates like axum may be 📌
-3. Break it trying to optimize something 📌
-4. Add TUI using ratatui (just because i like to try) 📌
+  - tls and security - 📌
+2. Add TUI using ratatui (but first just with crossterm) ⏳
+3. Rewrite it using some external crates like axum may be 📌
+4. Break it trying to optimize something 📌
 5. ... "no tengo ni una puta idea" what i will do next
--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,7 @@
 use std::{
     error::Error,
     io::{Write, stdout},
+    process::exit,
     thread,
     time::Duration,
 };
@@ -8,17 +9,20 @@ use std::{
 use crossterm::{
     QueueableCommand,
     cursor::MoveTo,
-    event::{Event, poll, read},
+    event::{Event, KeyCode, KeyModifiers, poll, read},
     terminal::{self, Clear},
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hi buddy");
     let mut stdout = stdout();
+    let _ = terminal::enable_raw_mode()?;
     let (mut w, mut h) = terminal::size()?;
-    let bar = "-";
+    let bar = "─";
     let mut border_line = bar.repeat(w as usize);
-    loop {
+    let mut prompt = String::new();
+    let mut exit = false;
+    while !exit {
         while poll(Duration::ZERO)? {
             match read()? {
                 Event::Resize(nw, nh) => {
@@ -26,7 +30,18 @@ fn main() -> Result<(), Box<dyn Error>> {
                     h = nh;
                     border_line = bar.repeat(w as usize);
                 }
-                Event::Key(event) => todo!(),
+                Event::Key(event) => match event.code {
+                    KeyCode::Char(c) => {
+                        if c == 'c' && event.modifiers.contains(KeyModifiers::CONTROL) {
+                            exit = true;
+                        }
+                        prompt.push(c);
+                    }
+                    KeyCode::Backspace => {
+                        prompt.pop();
+                    }
+                    _ => {}
+                },
                 _ => {}
             }
         }
@@ -35,8 +50,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         stdout.queue(Clear(terminal::ClearType::All))?;
         stdout.queue(MoveTo(0, h - 2))?;
         stdout.write(border_line.as_bytes())?;
+        stdout.write(prompt.as_bytes())?;
         stdout.queue(MoveTo(0, h - 1))?;
         stdout.flush()?;
         thread::sleep(Duration::from_millis(16));
     }
+    Ok(())
 }
