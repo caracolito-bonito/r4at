e0d1e8ff8209956ead8245a21689ab8a73b38406
Sergey Kupriyanov <xvoztuk@gmail.com>
2026-05-24 15:50:02 +0400

    ok, now it can draw a border for input part of the client

--- src/client.rs
+++ src/client.rs
@@ -7,16 +7,36 @@ use std::{
 
 use crossterm::{
     QueueableCommand,
-    cursor::{self, MoveRight, MoveTo},
+    cursor::MoveTo,
+    event::{Event, poll, read},
     terminal::{self, Clear},
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello from client");
+    println!("Hi buddy");
     let mut stdout = stdout();
-    let (width, height) = terminal::size()?;
-    stdout.queue(Clear(crossterm::terminal::ClearType::All))?;
-    stdout.flush()?;
-    stdout.queue(MoveTo(width / 2, height / 2))?;
-    Ok(())
+    let (mut w, mut h) = terminal::size()?;
+    let bar = "-";
+    let mut border_line = bar.repeat(w as usize);
+    loop {
+        while poll(Duration::ZERO)? {
+            match read()? {
+                Event::Resize(nw, nh) => {
+                    w = nw;
+                    h = nh;
+                    border_line = bar.repeat(w as usize);
+                }
+                Event::Key(event) => todo!(),
+                _ => {}
+            }
+        }
+
+        // rendering chat window
+        stdout.queue(Clear(terminal::ClearType::All))?;
+        stdout.queue(MoveTo(0, h - 2))?;
+        stdout.write(border_line.as_bytes())?;
+        stdout.queue(MoveTo(0, h - 1))?;
+        stdout.flush()?;
+        thread::sleep(Duration::from_millis(16));
+    }
 }
