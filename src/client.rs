e938bf227779481600045c276ffd8f178b70e2cb
Sergey Kupriyanov <xvoztuk@gmail.com>
2026-06-20 21:11:00 +0400

    feature: rendering of chat window with messages trimming

--- src/client.rs
+++ src/client.rs
@@ -12,6 +12,29 @@ use crossterm::{
     terminal::{self, Clear},
 };
 
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+fn chat_window(
+    stdout: &mut impl Write,
+    messages: &[String],
+    boundary: Rect,
+) -> Result<(), Box<dyn Error>> {
+    let len = messages.len();
+
+    let extra = len.checked_sub(boundary.h as usize).unwrap_or(0);
+    for (dy, line) in messages.iter().skip(extra).enumerate() {
+        stdout.queue(MoveTo(boundary.x, boundary.y + dy as u16))?;
+        let bytes = line.as_bytes();
+        stdout.write(bytes.get(0..boundary.w as usize).unwrap_or(bytes))?;
+    }
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hi buddy");
     let mut stdout = stdout();
@@ -21,6 +44,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut border_line = bar.repeat(w as usize);
     let mut prompt = String::new();
     let mut exit = false;
+    let mut chat = Vec::new();
     while !exit {
         while poll(Duration::ZERO)? {
             match read()? {
@@ -40,6 +64,10 @@ fn main() -> Result<(), Box<dyn Error>> {
                     KeyCode::Backspace => {
                         prompt.pop();
                     }
+                    KeyCode::Enter => {
+                        chat.push(prompt.clone());
+                        prompt.clear();
+                    }
                     _ => {}
                 },
                 _ => {}
@@ -48,6 +76,18 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // rendering chat window
         stdout.queue(Clear(terminal::ClearType::All))?;
+
+        chat_window(
+            &mut stdout,
+            &chat,
+            Rect {
+                x: 0,
+                y: 0,
+                w,
+                h: h - 2,
+            },
+        )?;
+
         stdout.queue(MoveTo(0, h - 2))?;
         stdout.write(border_line.as_bytes())?;
         stdout.write(prompt.as_bytes())?;
@@ -55,5 +95,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         stdout.flush()?;
         thread::sleep(Duration::from_millis(16));
     }
+    terminal::disable_raw_mode()?;
     Ok(())
 }
